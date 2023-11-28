6e622fa07e3e41485d22314f6c29910a9c0524cd
Даниил <145453928+Daniil20034548@users.noreply.github.com>
2023-11-28 07:45:19 +0300

    7.7 Взаимодействие объектов

---
+++ src/main/java/ru/oop/Ball.java
@@ -0,0 +1,12 @@
+package ru.oop;
+
+public class Ball {
+    public void tryRun(boolean condition) {
+        if (condition == true) {
+            System.out.println("Колобок съеден");
+        } else {
+            System.out.println("Колобок сбежал");
+        }
+    }
+}
+
