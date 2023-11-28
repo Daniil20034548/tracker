67abb7cd5d6f24da20d8a6258ae36730d2674971
Даниил <145453928+Daniil20034548@users.noreply.github.com>
2023-11-28 12:08:45 +0300

    7.8 Конструктор

--- src/main/java/ru/oop/Error.java
+++ src/main/java/ru/oop/Error.java
@@ -5,7 +5,7 @@ public class Error {
     private int status;
     private String message;
 
-    public Error(){
+    public Error() {
     }
 
     public Error(boolean active, int status, String message) {
@@ -15,13 +15,15 @@ public class Error {
     }
 
     public void printInfo() {
-        System.out.println(active + " " + status + " " + message);
+        System.out.println("Вам " + status + " лет?");
+        System.out.println("Это " + active);
+        System.out.println("Вам " + message + " " + status + " лет");
     }
 
     public static void main(String[] args) {
         Error error = new Error();
-        Error error1 = new Error(true, 10, "Hallow");
-        Error error2 = new Error(false, 15, "World");
+        Error error1 = new Error(true, 15, "действительно");
+        Error error2 = new Error(true, 10, "точно");
         error.printInfo();
         error1.printInfo();
         error2.printInfo();
