cf890180659d9ce6d032e87ebe53443a6237f977
Даниил <145453928+Daniil20034548@users.noreply.github.com>
2023-12-04 22:14:43 +0300

    9.3 Массивы и модели

---
+++ src/main/java/ru/pojo/Library.java
@@ -0,0 +1,33 @@
+package ru.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book book = new Book("book1", 200);
+        Book book1 = new Book("book2", 500);
+        Book book2 = new Book("book3", 100);
+        Book book3 = new Book("Clean code", 300);
+        Book[] books = new Book[4];
+        books[0] = book;
+        books[1] = book1;
+        books[2] = book2;
+        books[3] = book3;
+        for (int i = 0; i < books.length; i++) {
+            Book bk = books[i];
+            System.out.println(bk.getName() + " - " + bk.getPages());
+        }
+        Book rs = books[0];
+        books[0] = books[2];
+        books[2] = rs;
+        for (int i = 0; i < books.length; i++) {
+            Book bk = books[i];
+            System.out.println(bk.getName() + " - " + bk.getPages());
+        }
+        System.out.println("Найти книгу Clean code");
+        for (int i = 0; i < books.length; i++) {
+           Book bk = books[i];
+           if (bk.getName() == "Clean code") {
+               System.out.println(bk.getName() + " - " + bk.getPages());
+           }
+        }
+    }
+}
