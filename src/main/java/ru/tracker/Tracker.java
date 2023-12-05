e5777b4dd611b9ded6b71f83120d11594ecd1928
Даниил <145453928+Daniil20034548@users.noreply.github.com>
2023-12-05 13:58:58 +0300

    9.7 Tracker - хранилище

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -1,5 +1,9 @@
 package ru.tracker;
 
+
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +13,39 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] copy = new Item[items.length];
+        int size = 0;
+        for (Item cell : items) {
+            if (cell != null) {
+                copy[size] = cell;
+                size++;
+
+            } else {
+                break;
+            }
+        }
+        copy = Arrays.copyOf(copy, size);
+        return copy;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] copy = new Item[items.length];
+        int size = 0;
+        for (Item cell : items) {
+            if (cell != null) {
+                if (Objects.equals(cell.getName(), key)) {
+                    copy[size] = cell;
+                    size++;
+                }
+            } else {
+                break;
+            }
+        }
+        copy = Arrays.copyOf(copy, size);
+        return copy;
     }
 
     public Item findById(int id) {
