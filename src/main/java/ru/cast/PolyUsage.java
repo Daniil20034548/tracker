b52798ef1b583ea3ac3f8c826ab510a037b9a3b5
Даниил <145453928+Daniil20034548@users.noreply.github.com>
2023-12-13 17:22:13 +0300

    10.15 Зачем нужно приведение типов

---
+++ src/main/java/ru/cast/PolyUsage.java
@@ -0,0 +1,13 @@
+package ru.cast;
+
+public class PolyUsage {
+    public static void main(String[] args) {
+        Vehicle airplane = new Airplane();
+        Vehicle train = new Train();
+        Vehicle bus = new Bus();
+        Vehicle[] vehicles = new Vehicle[]{airplane, train, bus};
+        for (Vehicle i : vehicles) {
+            i.move();
+        }
+    }
+}
