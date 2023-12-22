e1d3daf568c40e9374b7c983cef132d719cdf9d6
Даниил <145453928+Daniil20034548@users.noreply.github.com>
2023-12-22 15:47:48 +0300

    11.5 Пользовательские исключения

---
+++ src/main/java/ru/ex/ElementNotFoundException.java
@@ -0,0 +1,27 @@
+package ru.ex;
+
+public class ElementNotFoundException extends Exception {
+        public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+            int result = -1;
+            for (int i = 0; i < value.length; i++) {
+                if (value[i] == key) {
+                    result = i;
+                }
+            }
+            if (result == -1) {
+                throw new ElementNotFoundException();
+            }
+            return result;
+        }
+
+        public static void main(String[] args) {
+            try {
+                String[] value = {"1", "2", "ret"};
+                System.out.println(indexOf(value, "ret"));
+            } catch (ElementNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
