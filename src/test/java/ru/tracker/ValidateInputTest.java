a4e8051ddf443fdb7ee4dba196be2a9e8ad2c150
Даниил <145453928+Daniil20034548@users.noreply.github.com>
2023-12-25 16:46:38 +0300

    11.11 Тест на ValidateInput

---
+++ src/test/java/ru/tracker/ValidateInputTest.java
@@ -0,0 +1,56 @@
+package ru.tracker;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ValidateInputTest {
+
+    @Test
+    void whenInvalidInput() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    void whenInvalidInput1() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[] {"1"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    void whenInvalidInput1And2And3() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[] {"1","2","3"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+        int selected1 = input.askInt("Enter menu:");
+        assertThat(selected1).isEqualTo(2);
+        int selected2 = input.askInt("Enter menu:");
+        assertThat(selected2).isEqualTo(3);
+    }
+
+    @Test
+    void whenInvalidInputMinus1() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[] {"-1"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(-1);
+
+    }
+}
