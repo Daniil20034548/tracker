7caf6e210c1db6a5bca23ef23a838304e515d8b1
Даниил <145453928+Daniil20034548@users.noreply.github.com>
2024-01-16 13:36:40 +0300

    13.4 Телефонный справочник

---
+++ src/main/java/ru/search/PhoneDictionary.java
@@ -0,0 +1,26 @@
+package ru.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (key.contains(person.getName())
+                    || key.contains(person.getSurname())
+                    || key.contains(person.getPhone())
+                    || key.contains(person.getAddress())) {
+                result.add(person);
+                break;
+            }
+        }
+        return result;
+    }
+}
+
---
+++ src/test/java/ru/search/PhoneDictionaryTest.java
@@ -0,0 +1,28 @@
+package ru.search;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Ivan", "Ivanov", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Ivan");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Ivanov");
+    }
+    @Test
+    public void whenFindByZero() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Ivan", "Ivanov", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("1111");
+        assertThat(persons.size()).isZero();
+    }
+}
