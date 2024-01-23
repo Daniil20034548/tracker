50211de3e37a9e924bde14cbd943c57f2765f154
Даниил <145453928+Daniil20034548@users.noreply.github.com>
2024-01-23 10:18:26 +0300

    14.3 Идентичные тексты

---
+++ src/main/java/ru/collection/UniqueText.java
@@ -0,0 +1,21 @@
+package ru.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean result = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>(Arrays.asList(origin));
+        for (String texts : text) {
+            if (!check.contains(texts)) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
