beb338f0a3de993c420cb2da1c67e4e473642d76
Даниил <145453928+Daniil20034548@users.noreply.github.com>
2024-05-23 18:20:43 +0300

    20.10 Подсчет функции в диапазоне

--- src/test/java/ru/function/FunctionCalculatorTest.java
+++ src/test/java/ru/function/FunctionCalculatorTest.java
@@ -10,7 +10,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
  public class FunctionCalculatorTest  {
     @Test
     public void whenLinearFunctionThenLinearResults() {
-        FunctionCalculator  function = new FunctionCalculator ();
+        FunctionCalculator  function = new FunctionCalculator();
         List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
         List<Double> expected = Arrays.asList(11D, 13D, 15D);
         assertThat(result, is(expected));
@@ -18,7 +18,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
     @Test
     public void whenSquareFunctionThenSquareResults() {
-        FunctionCalculator  function = new FunctionCalculator ();
+        FunctionCalculator  function = new FunctionCalculator();
         List<Double> result = function.diapason(1, 6, x -> 3 * x * x + 2 * x + 1);
         List<Double> expected = Arrays.asList(6D, 17D, 34D, 57D, 86D);
         assertThat(result, is(expected));
@@ -26,7 +26,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
     @Test
     public void whenIndicativeFunctionThenIndicativeResults() {
-        FunctionCalculator  function = new FunctionCalculator ();
+        FunctionCalculator  function = new FunctionCalculator();
         List<Double> result = function.diapason(3, 6, x -> Math.pow(2, x));
         List<Double> expected = Arrays.asList(8D, 16D, 32D);
         assertThat(result, is(expected));
