803932989f5d0738f97cdf6908bf4a828486e26e
Даниил <145453928+Daniil20034548@users.noreply.github.com>
2024-06-15 15:14:26 +0300

    21.7 Уникальность элементов и сортировка

--- src/main/java/ru/stream/Profiles.java
+++ src/main/java/ru/stream/Profiles.java
@@ -1,13 +1,16 @@
 package ru.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
 
-    public static List<Address> collect(List<Profile> profiles) {
+    public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
+                .sorted(Comparator.comparing(x -> x.getCity()))
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
