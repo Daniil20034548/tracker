1c9c0b32847dba36ae466abdb539e2731bf7f58b
Даниил <145453928+Daniil20034548@users.noreply.github.com>
2024-06-15 14:58:34 +0300

    21.4 Перенос методов в Stream API

--- src/main/java/ru/stream/ProductLabel.java
+++ src/main/java/ru/stream/ProductLabel.java
@@ -6,7 +6,8 @@ import java.util.stream.Collectors;
 public class ProductLabel {
     public List<String> generateLabels(List<Product> products) {
         return products.stream()
-                .filter(p -> p.getStandard() - p.getActual() >= 0 && p.getStandard() - p.getActual() <= 3)
+                .filter(p -> p.getStandard() - p.getActual() >= 0
+                        && p.getStandard() - p.getActual() <= 3)
                 .map(p -> new Label(p.getName(), p.getPrice() / 2))
                 .map(p -> p.toString())
                 .collect(Collectors.toList());
